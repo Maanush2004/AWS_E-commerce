20128012125e805501d2b9267faf05f54884c120
Maanush Putcha <p.maanush@gmail.com>
2024-11-14 10:34:19 +0530

    Footer: Don't render in invoice page
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,25 @@
 import Footer from "./Footer";
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Layout({children}) {
+    const pathname = usePathname();
+    const [footerVisibility, setFooterVisibility] = useState(true);
+    
+    useEffect(() => {
+        if (pathname.includes("/invoice")) {
+            setFooterVisibility(false)
+        } else {
+            setFooterVisibility(true)
+        }
+    }, [pathname])
+    
     return (
         <div>
             <div className="p-5">
                 {children}
             </div>
-            <Footer />
+            {footerVisibility && <Footer />}
         </div>
     );
 }
